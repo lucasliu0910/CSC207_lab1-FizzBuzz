98192df46f26392947b74847ef1f376059fe0ca4
Lucas Liu <lucasliu0910@gmail.com>
2026-09-24 15:32:48 -0400

    Update

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -8,7 +8,6 @@ class FizzBuzz {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 100; i++) {
-
             doFizzBuzz(i);
         }
     }
---
+++ src/lab1/Multiples.java
@@ -0,0 +1,17 @@
+package lab1;
+
+public class Multiples {
+
+    public static void main() {
+
+
+    }
+
+    public static int multiples(int n, int a, int b) {
+
+
+    }
+
+
+
+}
